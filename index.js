818bdda35aed7d11fe5cf22bfe3d6c72cb6207b4
ElyssaW <ejwinch1@gmail.com>
2021-05-03 18:33:16 -0500

    Wrote out classes for every piece

--- index.js
+++ index.js
@@ -16,36 +16,120 @@ var __extends = (this && this.__extends) || (function () {
 console.log('Hello');
 // Initialize board
 function createBoard() {
-    var board = [];
+    var newBoard = [];
     // 2D array of rows/columns
     for (var i = 0; i < 8; i++) {
         // Holding objects, currently set to empty, but will contain pieces as they move around
-        board.push(new Array([{}, {}, {}, {}, {}, {}, {}, {}]));
+        newBoard.push(new Array([{}, {}, {}, {}, {}, {}, {}, {}]));
     }
-    return board;
+    return newBoard;
 }
-// Set board to initial state
-function setStartingBoard() {
-    // Function set pawns
-    function setPawns() {
-        return;
-    }
-    // Function to set other pieces
-    var board = createBoard();
-    board.forEach(function (row, index) {
-        if (index === 0) {
+var board = createBoard();
+function checkBoundary(x, y) {
+    if (x >= 0 && x < 8) {
+        if (y >= 0 && y < 8) {
+            return true;
         }
-    });
+    }
+    return false;
+}
+function checkSquare(x, y) {
+    if (board[x][y]) {
+        return board[x][y];
+    }
+    else {
+        return false;
+    }
+}
+function checkCol(x, y, pace) {
+    var moves = [];
+    for (var i = 0; i < pace; i++) {
+        if (checkBoundary(x, y + i)) {
+            moves.push({ x: x, y: y + i });
+            if (checkSquare(x, y + i)) {
+                break;
+            }
+        }
+    }
+    for (var i = 0; i < pace; i++) {
+        if (checkBoundary(x, y - i)) {
+            moves.push({ x: x, y: y - i });
+            if (checkSquare(x, y - i)) {
+                break;
+            }
+        }
+    }
+    return moves;
+}
+function checkRow(x, y, pace) {
+    var moves = [];
+    for (var i = 0; i < pace; i++) {
+        if (checkBoundary(x + i, y)) {
+            moves.push({ x: x + i, y: y });
+            if (checkSquare(x + i, y)) {
+                break;
+            }
+        }
+    }
+    for (var i = 0; i < 8; i++) {
+        if (checkBoundary(x - i, y)) {
+            moves.push({ x: x - i, y: y });
+            if (checkSquare(x - i, y)) {
+                break;
+            }
+        }
+    }
+    return moves;
+}
+function checkDiagonal(x, y, pace) {
+    var moves = [];
+    for (var i = 0; i < pace; i++) {
+        if (checkBoundary(x - i, y + i)) {
+            moves.push({ x: x - i, y: y + i });
+            if (checkSquare(x - i, y + i)) {
+                break;
+            }
+        }
+    }
+    for (var i = 0; i < pace; i++) {
+        if (checkBoundary(x + i, y - i)) {
+            moves.push({ x: x + i, y: y - i });
+            if (checkSquare(x + i, y - i)) {
+                break;
+            }
+        }
+    }
+    for (var i = 0; i < pace; i++) {
+        if (checkBoundary(x - i, y - i)) {
+            moves.push({ x: x - i, y: y - i });
+            if (checkSquare(x - i, y - i)) {
+                break;
+            }
+        }
+    }
+    for (var i = 0; i < pace; i++) {
+        if (checkBoundary(x - i, y - i)) {
+            moves.push({ x: x - i, y: y - i });
+            if (checkSquare(x - i, y - i)) {
+                break;
+            }
+        }
+    }
+    return moves;
 }
 // Initialize pieces
 var Piece = /** @class */ (function () {
-    function Piece(name, color, pos) {
+    function Piece(name, color, pos, pace) {
+        // Whether the piece is alive or dead
+        this.alive = true;
+        // What spaces can the piece move to
+        this.availableMoves = [];
         this.name = name;
         this.color = color;
         this.pos = pos;
-        this.alive = true;
+        this.pace = pace;
     }
-    Piece.prototype.move = function () {
+    Piece.prototype.generateMoves = function () {
         // How the piece moves
     };
     return Piece;
@@ -53,19 +137,184 @@ var Piece = /** @class */ (function () {
 // Pawn
 var Pawn = /** @class */ (function (_super) {
     __extends(Pawn, _super);
-    function Pawn() {
-        return _super !== null && _super.apply(this, arguments) || this;
+    function Pawn(color, pos, pace) {
+        var _this = _super.call(this, 'Pawn', color, pos, pace) || this;
+        _this.generateMoves = function () {
+            if (_this.color === 'white') {
+                if (checkBoundary(_this.pos.x, _this.pos.y++) && !checkSquare(_this.pos.x, _this.pos.y++)) {
+                    _this.availableMoves.push({
+                        x: _this.pos.x,
+                        y: _this.pos.y++
+                    });
+                }
+                if (checkBoundary(_this.pos.x++, _this.pos.y++) && checkSquare(_this.pos.x++, _this.pos.y++)) {
+                    _this.availableMoves.push({
+                        x: _this.pos.x++,
+                        y: _this.pos.y++
+                    });
+                }
+                if (checkBoundary(_this.pos.x--, _this.pos.y++) && checkSquare(_this.pos.x--, _this.pos.y++)) {
+                    _this.availableMoves.push({
+                        x: _this.pos.x--,
+                        y: _this.pos.y++
+                    });
+                }
+            }
+            else {
+                if (checkBoundary(_this.pos.x, _this.pos.y--) && !checkSquare(_this.pos.x, _this.pos.y--)) {
+                    _this.availableMoves.push({
+                        x: _this.pos.x,
+                        y: _this.pos.y--
+                    });
+                }
+                if (checkBoundary(_this.pos.x++, _this.pos.y--) && checkSquare(_this.pos.x++, _this.pos.y--)) {
+                    _this.availableMoves.push({
+                        x: _this.pos.x++,
+                        y: _this.pos.y--
+                    });
+                }
+                if (checkBoundary(_this.pos.x--, _this.pos.y--) && checkSquare(_this.pos.x--, _this.pos.y--)) {
+                    _this.availableMoves.push({
+                        x: _this.pos.x--,
+                        y: _this.pos.y--
+                    });
+                }
+            }
+        };
+        return _this;
     }
     return Pawn;
 }(Piece));
 // Rook
+var Rook = /** @class */ (function (_super) {
+    __extends(Rook, _super);
+    function Rook(color, pos, pace) {
+        var _this = _super.call(this, 'Rook', color, pos, pace) || this;
+        _this.generateMoves = function () {
+            _this.availableMoves = _this.availableMoves.concat(checkCol(_this.pos.x, _this.pos.y, _this.pace));
+            _this.availableMoves = _this.availableMoves.concat(checkRow(_this.pos.x, _this.pos.y, _this.pace));
+        };
+        return _this;
+    }
+    return Rook;
+}(Piece));
 // Knight
+var Knight = /** @class */ (function (_super) {
+    __extends(Knight, _super);
+    function Knight(color, pos, pace) {
+        var _this = _super.call(this, 'Knight', color, pos, pace) || this;
+        _this.generateMoves = function () {
+            if (checkBoundary(_this.pos.x + 1, _this.pos.y + 2) && checkSquare(_this.pos.x + 1, _this.pos.y + 2)) {
+                _this.availableMoves.push({ x: _this.pos.x + 1, y: _this.pos.y + 2 });
+            }
+            if (checkBoundary(_this.pos.x + 2, _this.pos.y + 1) && checkSquare(_this.pos.x + 2, _this.pos.y + 1)) {
+                _this.availableMoves.push({ x: _this.pos.x + 2, y: _this.pos.y + 1 });
+            }
+            //===============
+            if (checkBoundary(_this.pos.x - 1, _this.pos.y + 2) && checkSquare(_this.pos.x - 1, _this.pos.y + 2)) {
+                _this.availableMoves.push({ x: _this.pos.x - 1, y: _this.pos.y + 2 });
+            }
+            if (checkBoundary(_this.pos.x - 2, _this.pos.y + 1) && checkSquare(_this.pos.x - 2, _this.pos.y + 1)) {
+                _this.availableMoves.push({ x: _this.pos.x - 2, y: _this.pos.y + 1 });
+            }
+            //===============
+            if (checkBoundary(_this.pos.x + 1, _this.pos.y - 2) && checkSquare(_this.pos.x + 1, _this.pos.y - 2)) {
+                _this.availableMoves.push({ x: _this.pos.x + 1, y: _this.pos.y - 2 });
+            }
+            if (checkBoundary(_this.pos.x + 2, _this.pos.y - 1) && checkSquare(_this.pos.x + 2, _this.pos.y - 1)) {
+                _this.availableMoves.push({ x: _this.pos.x + 2, y: _this.pos.y - 1 });
+            }
+            //================
+            if (checkBoundary(_this.pos.x - 1, _this.pos.y - 2) && checkSquare(_this.pos.x - 1, _this.pos.y - 2)) {
+                _this.availableMoves.push({ x: _this.pos.x - 1, y: _this.pos.y - 2 });
+            }
+            if (checkBoundary(_this.pos.x - 2, _this.pos.y - 1) && checkSquare(_this.pos.x - 2, _this.pos.y - 1)) {
+                _this.availableMoves.push({ x: _this.pos.x - 2, y: _this.pos.y - 1 });
+            }
+        };
+        return _this;
+    }
+    return Knight;
+}(Piece));
 // Bishop
+var Bishop = /** @class */ (function (_super) {
+    __extends(Bishop, _super);
+    function Bishop(color, pos, pace) {
+        var _this = _super.call(this, 'Bishop', color, pos, pace) || this;
+        _this.generateMoves = function () {
+            _this.availableMoves = _this.availableMoves.concat(checkDiagonal(_this.pos.x, _this.pos.y, _this.pace));
+        };
+        return _this;
+    }
+    return Bishop;
+}(Piece));
 // Queen
+var Queen = /** @class */ (function (_super) {
+    __extends(Queen, _super);
+    function Queen(color, pos, pace) {
+        var _this = _super.call(this, 'Queen', color, pos, pace) || this;
+        _this.generateMoves = function () {
+            _this.availableMoves = _this.availableMoves.concat(checkDiagonal(_this.pos.x, _this.pos.y, _this.pace));
+            _this.availableMoves = _this.availableMoves.concat(checkCol(_this.pos.x, _this.pos.y, _this.pace));
+            _this.availableMoves = _this.availableMoves.concat(checkRow(_this.pos.x, _this.pos.y, _this.pace));
+        };
+        return _this;
+    }
+    return Queen;
+}(Piece));
 // King
+var King = /** @class */ (function (_super) {
+    __extends(King, _super);
+    function King(color, pos, pace) {
+        var _this = _super.call(this, 'King', color, pos, pace) || this;
+        _this.generateMoves = function () {
+            _this.availableMoves = _this.availableMoves.concat(checkDiagonal(_this.pos.x, _this.pos.y, _this.pace));
+            _this.availableMoves = _this.availableMoves.concat(checkCol(_this.pos.x, _this.pos.y, _this.pace));
+            _this.availableMoves = _this.availableMoves.concat(checkRow(_this.pos.x, _this.pos.y, _this.pace));
+        };
+        return _this;
+    }
+    return King;
+}(Piece));
 // Check position
 // Checkmate position
-// Player class
-// Color
-// Pieces
 // Turn counter
+// Player class
+var Player = /** @class */ (function () {
+    function Player(name, color) {
+        this.pieces = [];
+        this.capturedPieces = [];
+        this.name = name;
+        this.color = color;
+    }
+    Player.prototype.createPieces = function (backRow, frontRow) {
+        // Create Pawns
+        this.pieces.push(new Pawn(this.color, { x: 0, y: frontRow }, 1));
+        this.pieces.push(new Pawn(this.color, { x: 1, y: frontRow }, 1));
+        this.pieces.push(new Pawn(this.color, { x: 2, y: frontRow }, 1));
+        this.pieces.push(new Pawn(this.color, { x: 3, y: frontRow }, 1));
+        this.pieces.push(new Pawn(this.color, { x: 4, y: frontRow }, 1));
+        this.pieces.push(new Pawn(this.color, { x: 5, y: frontRow }, 1));
+        this.pieces.push(new Pawn(this.color, { x: 6, y: frontRow }, 1));
+        this.pieces.push(new Pawn(this.color, { x: 7, y: frontRow }, 1));
+        // Create other pieces
+        // Rooks
+        this.pieces.push(new Rook(this.color, { x: 0, y: backRow }, board.length));
+        this.pieces.push(new Rook(this.color, { x: 7, y: backRow }, board.length));
+        //Knights
+        this.pieces.push(new Knight(this.color, { x: 1, y: backRow }, board.length));
+        this.pieces.push(new Knight(this.color, { x: 6, y: backRow }, board.length));
+        // Bishops
+        this.pieces.push(new Bishop(this.color, { x: 2, y: backRow }, board.length));
+        this.pieces.push(new Bishop(this.color, { x: 5, y: backRow }, board.length));
+        // Queen
+        this.pieces.push(new Queen(this.color, { x: 4, y: backRow }, board.length));
+        // King
+        this.pieces.push(new King(this.color, { x: 5, y: backRow }, 1));
+    };
+    return Player;
+}());
+var playerOne = new Player('Elyssa', 'white');
+playerOne.createPieces(0, 1);
+var playerTwo = new Player('Guest', 'black');
+playerTwo.createPieces(7, 6);
--- index.ts
+++ index.ts
@@ -2,33 +2,133 @@ console.log('Hello')
 
 // Initialize board
 function createBoard() {
-    let board = []
+    let newBoard = []
 
     // 2D array of rows/columns
     for (let i = 0; i < 8; i++) {
         // Holding objects, currently set to empty, but will contain pieces as they move around
-        board.push(new Array([ {}, {}, {}, {}, {}, {}, {}, {} ]))
+        newBoard.push(new Array([ {}, {}, {}, {}, {}, {}, {}, {} ]))
     }
 
-    return board
+    return newBoard
 }
 
-// Set board to initial state
-function setStartingBoard() {
+let board = createBoard()
 
-    // Function set pawns
-    function setPawns () {
-        return 
+function checkBoundary(x: number, y: number) {
+    if (x >= 0 && x < 8) {
+        if (y >=0 && y < 8) {
+            return true
+        }
     }
 
-    // Function to set other pieces
-    let board = createBoard()
+    return false
+}
 
-    board.forEach((row, index) => {
-        if (index === 0) {
+function checkSquare(x: number, y: number) {
+    if (board[x][y]) {
+        return board[x][y]
+    } else {
+        return false
+    }
+}
 
+function checkCol(x: number, y: number, pace:number) {
+    let moves = []
+
+    for (let i = 0; i < pace; i++) {
+        if (checkBoundary(x, y+i)) {
+            moves.push({x: x, y: y+i})
+
+            if (checkSquare(x, y+i)) {
+                break;
+            }
         }
-    })
+    }
+
+    for (let i = 0; i < pace; i++) {
+        if (checkBoundary(x, y-i)) {
+            moves.push({x: x, y: y-i})
+
+            if (checkSquare(x, y-i)) {
+                break;
+            }
+        }
+    }
+
+    return moves
+}
+
+function checkRow (x: number, y: number, pace:number) {
+    let moves = []
+
+    for (let i = 0; i < pace; i++) {
+        if (checkBoundary(x+i, y)) {
+            moves.push({x: x+i, y: y})
+
+            if (checkSquare(x+i, y)) {
+                break;
+            }
+        }
+    }
+
+    for (let i = 0; i < 8; i++) {
+        if (checkBoundary(x-i, y)) {
+            moves.push({x: x-i, y: y})
+
+            if (checkSquare(x-i, y)) {
+                break;
+            }
+        }
+    }
+
+    return moves
+}
+
+function checkDiagonal (x: number, y: number, pace:number) {
+    let moves = []
+
+    for (let i = 0; i < pace; i++) {
+        if (checkBoundary(x-i, y+i)) {
+            moves.push({x: x-i, y: y+i})
+
+            if (checkSquare(x-i, y+i)) {
+                break;
+            }
+        }
+    }
+
+    for (let i = 0; i < pace; i++) {
+        if (checkBoundary(x+i, y-i)) {
+            moves.push({x: x+i, y: y-i})
+
+            if (checkSquare(x+i, y-i)) {
+                break;
+            }
+        }
+    }
+
+    for (let i = 0; i < pace; i++) {
+        if (checkBoundary(x-i, y-i)) {
+            moves.push({x: x-i, y: y-i})
+
+            if (checkSquare(x-i, y-i)) {
+                break;
+            }
+        }
+    }
+
+    for (let i = 0; i < pace; i++) {
+        if (checkBoundary(x-i, y-i)) {
+            moves.push({x: x-i, y: y-i})
+
+            if (checkSquare(x-i, y-i)) {
+                break;
+            }
+        }
+    }
+
+    return moves
 }
 
 // Initialize pieces
@@ -45,40 +145,260 @@ class Piece {
     // Color or "team" of the piece
     color: string
 
+    // How many spaces the piece can move at once
+    pace: number
+
     // Whether the piece is alive or dead
-    alive: boolean
+    alive = true
 
     // What spaces can the piece move to
-    availableMoves: [{x: number, y: number}]
+    availableMoves = []
 
-    constructor(name: string, color: string, pos: {x: number, y: number}) {
+    constructor(name: string, color: string, pos: {x: number, y: number}, pace: number) {
         this.name = name
         this.color = color
         this.pos = pos
-        this.alive = true
+        this.pace = pace
     }
 
-    move() {
+    generateMoves() {
         // How the piece moves
     }
 }
 
 // Pawn
 class Pawn extends Piece {
-    name: 'Pawn'
+    constructor (color: string, pos: {x: number, y: number}, pace:number) {
+        super('Pawn', color, pos, pace)
+
+        this.generateMoves = () => {
+            if (this.color === 'white') {
+
+                if (checkBoundary(this.pos.x, this.pos.y++) && !checkSquare(this.pos.x, this.pos.y++)) {
+                    this.availableMoves.push({
+                        x: this.pos.x,
+                        y: this.pos.y++
+                    })
+                }
+
+                if (checkBoundary(this.pos.x++, this.pos.y++) && checkSquare(this.pos.x++, this.pos.y++)) {
+                    this.availableMoves.push({
+                        x: this.pos.x++,
+                        y: this.pos.y++
+                    })
+                }
+
+                if (checkBoundary(this.pos.x--, this.pos.y++) && checkSquare(this.pos.x--, this.pos.y++)) {
+                    this.availableMoves.push({
+                        x: this.pos.x--,
+                        y: this.pos.y++
+                    })
+                }
+
+            } else {
+
+                if (checkBoundary(this.pos.x, this.pos.y--) && !checkSquare(this.pos.x, this.pos.y--)) {
+                    this.availableMoves.push({
+                        x: this.pos.x,
+                        y: this.pos.y--
+                    })
+                }
+
+                if (checkBoundary(this.pos.x++, this.pos.y--) && checkSquare(this.pos.x++, this.pos.y--)) {
+                    this.availableMoves.push({
+                        x: this.pos.x++,
+                        y: this.pos.y--
+                    })
+                }
+
+                if (checkBoundary(this.pos.x--, this.pos.y--) && checkSquare(this.pos.x--, this.pos.y--)) {
+                    this.availableMoves.push({
+                        x: this.pos.x--,
+                        y: this.pos.y--
+                    })
+                }
+
+            }
+        }
+    }
 }
 
 // Rook
+class Rook extends Piece {
+    constructor (color: string, pos: {x: number, y: number}, pace:number) {
+        super('Rook', color, pos, pace)
+
+        this.generateMoves = () => {
+            this.availableMoves = this.availableMoves.concat(checkCol(this.pos.x,this.pos.y, this.pace))
+            this.availableMoves = this.availableMoves.concat(checkRow(this.pos.x,this.pos.y, this.pace))
+        }
+
+    }
+}
+
 // Knight
+class Knight extends Piece {
+    constructor (color: string, pos: {x: number, y: number}, pace:number) {
+        super('Knight', color, pos, pace)
+
+        this.generateMoves = () => {
+            if (checkBoundary(this.pos.x+1,this.pos.y+2) && checkSquare(this.pos.x+1,this.pos.y+2)) {
+                this.availableMoves.push({x: this.pos.x+1, y: this.pos.y+2})
+            }
+
+            if (checkBoundary(this.pos.x+2,this.pos.y+1) && checkSquare(this.pos.x+2,this.pos.y+1)) {
+                this.availableMoves.push({x: this.pos.x+2, y: this.pos.y+1})
+            }
+
+            //===============
+            if (checkBoundary(this.pos.x-1,this.pos.y+2) && checkSquare(this.pos.x-1,this.pos.y+2)) {
+                this.availableMoves.push({x: this.pos.x-1, y: this.pos.y+2})
+            }
+
+            if (checkBoundary(this.pos.x-2,this.pos.y+1) && checkSquare(this.pos.x-2,this.pos.y+1)) {
+                this.availableMoves.push({x: this.pos.x-2, y: this.pos.y+1})
+            }
+
+            //===============
+            if (checkBoundary(this.pos.x+1,this.pos.y-2) && checkSquare(this.pos.x+1,this.pos.y-2)) {
+                this.availableMoves.push({x: this.pos.x+1, y: this.pos.y-2})
+            }
+            
+            if (checkBoundary(this.pos.x+2,this.pos.y-1) && checkSquare(this.pos.x+2,this.pos.y-1)) {
+                this.availableMoves.push({x: this.pos.x+2, y: this.pos.y-1})
+            }
+
+            //================
+            if (checkBoundary(this.pos.x-1,this.pos.y-2) && checkSquare(this.pos.x-1,this.pos.y-2)) {
+                this.availableMoves.push({x: this.pos.x-1, y: this.pos.y-2})
+            }
+
+            if (checkBoundary(this.pos.x-2,this.pos.y-1) && checkSquare(this.pos.x-2,this.pos.y-1)) {
+                this.availableMoves.push({x: this.pos.x-2, y: this.pos.y-1})
+            }
+        }
+    }
+}
+
 // Bishop
+class Bishop extends Piece {
+    constructor (color: string, pos: {x: number, y: number}, pace:number) {
+        super('Bishop', color, pos, pace)
+
+        this.generateMoves = () => {
+            this.availableMoves = this.availableMoves.concat(checkDiagonal(this.pos.x,this.pos.y, this.pace))
+        }
+    }
+}
+
 // Queen
+class Queen extends Piece {
+    constructor (color: string, pos: {x: number, y: number}, pace:number) {
+        super('Queen', color, pos, pace)
+
+        this.generateMoves = () => {
+            this.availableMoves = this.availableMoves.concat(checkDiagonal(this.pos.x,this.pos.y, this.pace))
+            this.availableMoves = this.availableMoves.concat(checkCol(this.pos.x,this.pos.y, this.pace))
+            this.availableMoves = this.availableMoves.concat(checkRow(this.pos.x,this.pos.y, this.pace))
+        }
+    }
+}
+
 // King
+class King extends Piece {
+    constructor (color: string, pos: {x: number, y: number}, pace:number) {
+        super('King', color, pos, pace)
+
+        this.generateMoves = () => {
+            this.availableMoves = this.availableMoves.concat(checkDiagonal(this.pos.x,this.pos.y, this.pace))
+            this.availableMoves = this.availableMoves.concat(checkCol(this.pos.x,this.pos.y, this.pace))
+            this.availableMoves = this.availableMoves.concat(checkRow(this.pos.x,this.pos.y, this.pace))
+        }
+    }
+}
 
 // Check position
     // Checkmate position
 
-// Player class
-    // Color
-    // Pieces
-
 // Turn counter
+
+// Player class
+class Player {
+    name: string
+    color: string
+    pieces = []
+    capturedPieces = []
+
+    constructor(name: string, color: string) {
+        this.name = name
+        this.color = color
+    }
+
+    createPieces (backRow:number, frontRow:number) {
+        // Create Pawns
+        this.pieces.push(
+            new Pawn(this.color, {x:0, y:frontRow}, 1)
+        )
+        this.pieces.push(
+            new Pawn(this.color, {x:1, y:frontRow}, 1)
+        )
+        this.pieces.push(
+            new Pawn(this.color, {x:2, y:frontRow}, 1)
+        )
+        this.pieces.push(
+            new Pawn(this.color, {x:3, y:frontRow}, 1)
+        )
+        this.pieces.push(
+            new Pawn(this.color, {x:4, y:frontRow}, 1)
+        )
+        this.pieces.push(
+            new Pawn(this.color, {x:5, y:frontRow}, 1)
+        )
+        this.pieces.push(
+            new Pawn(this.color, {x:6, y:frontRow}, 1)
+        )
+        this.pieces.push(
+            new Pawn(this.color, {x:7, y:frontRow}, 1)
+        )
+
+        // Create other pieces
+        // Rooks
+        this.pieces.push(
+            new Rook(this.color, {x:0, y:backRow}, board.length)
+        )
+        this.pieces.push(
+            new Rook(this.color, {x:7, y:backRow}, board.length)
+        )
+
+        //Knights
+        this.pieces.push(
+            new Knight(this.color, {x:1, y:backRow}, board.length)
+        )
+        this.pieces.push(
+            new Knight(this.color, {x:6, y:backRow}, board.length)
+        )
+
+        // Bishops
+        this.pieces.push(
+            new Bishop(this.color, {x:2, y:backRow}, board.length)
+        )
+        this.pieces.push(
+            new Bishop(this.color, {x:5, y:backRow}, board.length)
+        )
+
+        // Queen
+        this.pieces.push(
+            new Queen(this.color, {x:4, y:backRow}, board.length)
+        )
+
+        // King
+        this.pieces.push(
+            new King(this.color, {x:5, y:backRow}, 1)
+        )
+    }
+}
+
+let playerOne = new Player('Elyssa', 'white')
+playerOne.createPieces(0, 1)
+let playerTwo = new Player('Guest', 'black')
+playerTwo.createPieces(7, 6)
